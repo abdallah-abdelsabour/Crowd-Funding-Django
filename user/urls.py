3dc0be1764d8bac5799ddaf3f77f5ef1e0da8790
Abdallah <Abdallah97158@gmail.com>
2023-03-08 20:34:48 +0200

    first commit

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("login",views.login),
+    path("signup",views.signup),
+    path("logout", views.logout),
+    path("delete" , views.delete_account)
+
+
+
+]
---
+++ user/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+def signup(request):
+    pass
+
+
+
+
+def login(request):
+    pass
+
+
+
+def update(request):
+    pass
+
+
+
+
+def logout(request):
+    pass
+
+
+def delete_account(request):
+    pass
