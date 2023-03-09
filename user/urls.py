26e46803492ecc4e66e92182d7a5f243a9b3c455
Abdallah <Abdallah97158@gmail.com>
2023-03-09 03:48:29 +0200

    email confiramtion done

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("login/",views.login),
-    path("signup",views.signup),
+    path("signup",views.signup,name="home"),
     path("logout", views.logout),
-    path("delete" , views.delete_account)
+    path("delete" , views.delete_account),
+     path('activate/<uidb64>/<token>', views.activate, name='activate')
 
 
 
