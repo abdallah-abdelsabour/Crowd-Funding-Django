f2fb66e7b172439b3594ec8f92228478f8193747
Abdallah <Abdallah97158@gmail.com>
2023-03-09 13:47:07 +0200

    login done

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,14 @@
 
-from django.urls import path
+from django.urls import path , include
 from . import views
 urlpatterns = [
-    path("login/",views.login),
-    path("signup",views.signup,name="home"),
-    path("logout", views.logout),
-    path("delete" , views.delete_account),
-     path('activate/<uidb64>/<token>', views.activate, name='activate')
+
+    path("", include("django.contrib.auth.urls")),
+    path("profile",views.profile_page,name="profile_page"),
+    path("signup",views.signup,name="signup"),
+    path("update",views.update,name="update"),
+    # path("delete" , views.delete_account),
+ path('activate/<uidb64>/<token>', views.activate, name='activate')
 
 
 
