27a185a29bc75b3d3a5942d3f00c2fbe6cce2af4
Abdallah <Abdallah97158@gmail.com>
2023-04-14 22:51:29 +0200

    update user date logeed

--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
 
     path("", include("django.contrib.auth.urls")),
-    path("profile",views.profile_page,name="profile_page"),
-    path("signup",views.signup,name="signup"),
-    path("update",views.update,name="update"),
+    path("profile/",views.profile_page,name="profile_page"),
+    path("signup/",views.signup,name="signup"),
+    path("update/",views.update,name="update"),
     # path("delete" , views.delete_account),
  path('activate/<uidb64>/<token>', views.activate, name='activate')
 
